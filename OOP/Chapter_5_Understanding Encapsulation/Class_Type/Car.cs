ff6b22ceca4ddbd73473c953d10ffb116ddb54e8
Shamil <kurbanov@outlook.de>
2023-08-23 15:38:36 +0200

    Add in OOP Class_Type , and class Car

---
+++ OOP/Chapter_5_Understanding Encapsulation/Class_Type/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace SimpleClassExample;
+
+public class Car
+{
+    //The 'state' of the Car
+    public string petName;
+    public int currSpeed;
+    
+    //The functionality of the Car
+    public void PrintState()
+    {
+        WriteLine($"{petName} is going {currSpeed} MPH");
+    }
+    public void SpeedUP(int delta) => currSpeed += delta;
+}
---
+++ OOP/Chapter_5_Understanding Encapsulation/Class_Type/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace SimpleClassExample;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Car myCar = new Car();
+        myCar.petName = "Henry";
+        myCar.currSpeed = 10;
+
+        for (int i = 0; i < 10; i++)
+        {
+            myCar.currSpeed += 5;
+            myCar.PrintState();
+        }
+    }
+}
+
