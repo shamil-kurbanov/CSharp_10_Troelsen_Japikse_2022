8e672b5c721859c18fad00aa67acc32b3c8e95b4
Shamil <kurbanov@outlook.de>
2023-09-04 14:05:05 +0200

    class Garage, update

--- OOP/Chapter_5_Understanding Encapsulation/AutoProps/Garage.cs
+++ OOP/Chapter_5_Understanding Encapsulation/AutoProps/Garage.cs
@@ -9,4 +9,25 @@ public class Garage{
 
     //The hidden Car backing field is set to null!
     public Car? MyAuto{get; set;}
+
+    // Must use constructors to override default values 
+    // assigned to hidden backing fields.
+    public Garage(){
+        MyAuto = new Car();
+        NumberOfCars = 1;
+    }
+
+    public Garage(Car car, int number){
+        MyAuto = car;
+        NumberOfCars = number;
+    }
+
+    /*
+     * With this modification, you can now place a Car object 
+     * into the Garage object and retrieve it later:
+    */
+    public void Display(){
+        WriteLine($"Number of cars: {NumberOfCars}");
+        WriteLine($"Your car is named: {MyAuto?.PetName ?? "NoName"}");
+    }
 }
--- OOP/Chapter_5_Understanding Encapsulation/AutoProps/Program.cs
+++ OOP/Chapter_5_Understanding Encapsulation/AutoProps/Program.cs
@@ -4,15 +4,22 @@ using AutoProps;
 
 WriteLine("***** Fun with Automatic Properties *****\n");
 
+// Make a car.
 Car car1 = new Car();
 car1.PetName = "Frank";
 car1.Speed = 55;
 car1.Color = "Red";
+car1.DisplayStats();
 
 WriteLine($"Your car is named {car1.PetName}? That's odd...");
 WriteLine("------------------------------------------------");
 car1.DisplayStats();
 WriteLine("");
 
+// Put car in the garage and print.
 Garage garage = new Garage();
-WriteLine($"Number of cars in garage: {garage.NumbersOfCars}");
+garage.MyAuto = car1; // Using the set accessor.
+WriteLine($"Number of cars in garage: {garage.NumberOfCars}");
+WriteLine($"Your car is named {garage.MyAuto?.PetName}");
+WriteLine("------------------------------------------------");
+
