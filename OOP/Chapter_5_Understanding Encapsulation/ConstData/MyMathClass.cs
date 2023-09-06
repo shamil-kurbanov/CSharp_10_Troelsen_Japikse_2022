19fda768120b18c2a23a925e1ac2e0c49042eb5b
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-09-06 16:20:11 +0200

    Add class MyMathClass with PI constant

---
+++ OOP/Chapter_5_Understanding Encapsulation/ConstData/MyMathClass.cs
@@ -0,0 +1,10 @@
+using System;
+using static System.Console;
+
+namespace ConstData
+{
+    public class MyMathClass
+    {
+        public const double PI = 3.14159;
+    }
+}
--- OOP/Chapter_5_Understanding Encapsulation/ConstData/Program.cs
+++ OOP/Chapter_5_Understanding Encapsulation/ConstData/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+using ConstData;
+
+WriteLine("***** Fun with Const *****\n");
+WriteLine($"The value of PI is {MyMathClass.PI}");
