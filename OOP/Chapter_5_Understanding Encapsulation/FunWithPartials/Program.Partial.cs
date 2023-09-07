f84723e682524796b67b7db9a19f1eed88c0ee5f
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-09-07 15:02:06 +0200

    Add partial class Program and function sayHello()

---
+++ OOP/Chapter_5_Understanding Encapsulation/FunWithPartials/Program.Partial.cs
@@ -0,0 +1,11 @@
+//this is the partial class
+using System;
+using static System.Console;
+
+public partial class Program
+{
+    public static string SayHello(string name)
+    {
+        return $"Hello, {name}!";
+    }
+}
---
+++ OOP/Chapter_5_Understanding Encapsulation/FunWithPartials/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine(SayHello("Shamil"));
+    }
+}
