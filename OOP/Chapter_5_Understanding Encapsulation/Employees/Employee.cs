9f0d19d66c342bc1548d8eb38b70bcb4683f7b37
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-09-13 14:25:48 +0200

    Copy Employee.cs, EmployeePayTypeEnum.cs and Employee.Core.cs from EmployeeApp project

---
+++ OOP/Chapter_5_Understanding Encapsulation/Employees/Employee.cs
@@ -0,0 +1,130 @@
+using System;
+using static System.Console;
+using EmployeeApp;
+using static EmployeeApp.EmployeePayTypeEnum;
+
+namespace EmployeeApp;
+
+class Employee
+{
+    //Field data
+    private string? _empName; //private field
+    private int _empID; 
+    public float _currPay;//public field
+    private int _empAge;
+
+    private string? _empSSN;
+
+    private EmployeePayTypeEnum _payType;
+
+    private DateTime _hireDate;
+
+    public DateTime HireDate{
+        get => _hireDate;
+        set => _hireDate = value;
+    }
+
+    //Properties!
+
+    public EmployeePayTypeEnum PayType{
+        get => _payType;
+        set => _payType = value;
+    }
+    public string? SocialSecurityNumber
+    {
+        get => _empSSN;
+        set => _empSSN = value;
+
+    }
+    public string? Name
+    {
+        get{ return _empName; }
+        set{
+            if(value.Length > 15){
+                WriteLine("Error! Name length exceeds 15 characters!");
+            }else{
+                _empName = value;
+            }
+        }
+    }
+
+    //the 'int' type is a value type, so it's passed by value
+    //the 'int' represents the type of data this property encapsulates
+    public int ID{
+        get{return _empID;}
+        set{_empID = value;}
+    }
+
+    public float Pay{
+        get{return _currPay;}
+        set{_currPay = value;}
+    }
+
+    public int Age{
+        get{return _empAge;}
+        set{_empAge = value;}
+    }
+
+    //Constructors
+
+    //Default constructor   
+    public Employee(){
+
+    }
+
+    //Custom constructor
+    public Employee(string? name, int id, float pay, string empSsn):this(name, 0, id, pay, empSsn, EmployeePayTypeEnum.Salaried){}
+    public Employee(string? name, int age, int id, float pay, string? Ssn, EmployeePayTypeEnum payType){
+        Name = name;
+        Age = age;
+        ID = id;
+        Pay = pay;
+        SocialSecurityNumber = Ssn; //assign the value of the ssn parameter to the field
+        PayType = payType;
+    }
+
+    //Methods
+    public void GiveBonus(float ammount)
+    {
+            Pay = this switch
+            {
+                {Age: >= 18, PayType: EmployeePayTypeEnum.Commission, HireDate.Year: > 2010}
+                    => Pay += .10F * ammount,
+                {Age: >= 18, PayType: EmployeePayTypeEnum.Hourly, HireDate.Year: > 2010}
+                    => Pay += 40F * ammount/2080F,
+                {Age: >= 18, PayType: EmployeePayTypeEnum.Salaried, HireDate.Year: > 2010}
+                    => Pay += ammount,
+                _ => Pay += 0
+            };
+    }
+
+    public void DisplayStats(){
+        WriteLine($"Name: {Name}");
+        WriteLine($"ID: {ID}");
+        WriteLine($"Age: {Age}");
+        WriteLine($"Pay: {Pay}");
+        WriteLine($"SSN: {SocialSecurityNumber}");
+    }
+
+    //Accessors (getters)
+    public string? GetName(){
+        return _empName;
+    }
+
+    //Mutators (setters)
+    public void SetName(string name){
+        //Do check on incomming value before making assignment
+
+        if(name.Length > 15){
+            WriteLine("Error! Name length exceeds 15 characters!");
+        } else{
+            _empName = name;
+        }
+    }
+
+    public int GetID(){
+        return _empID;
+    }
+
+
+}
---
+++ OOP/Chapter_5_Understanding Encapsulation/Employees/EmployeePayTypeEnum.cs
@@ -0,0 +1,10 @@
+using System;
+using static System.Console;
+
+namespace EmployeeApp;
+
+public enum EmployeePayTypeEnum{
+    Hourly,
+    Salaried,
+    Commission
+}
